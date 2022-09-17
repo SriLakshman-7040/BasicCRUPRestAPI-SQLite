56422f50265bd95cc8939214d593e603fd0d25c7
Sri Lakshman C <Srilakshman@softura.com>
2022-09-17 14:02:01 +0530

    Implement JWT and WebApp

---
+++ CRUD_WebApp_DepandApi/CRUD_WebApp_DepandApi/wwwroot/js/crudApi.js
@@ -0,0 +1,16 @@
+﻿
+$.get("https://localhost:7029/api/PeopleDB", function (data, status) {
+    if (data)
+    {
+        let code = "";
+        for (let value in data)
+        {
+            code += "<tr>"
+            code += "<td>" + data[value].sno + "</td>"
+            code += "<td>" + data[value].name + "</td>"
+            code += "<td>" + data[value].city + "</td>"
+            code += "</tr>";
+        }
+        $("#bindingData").html(code);
+    }
+})
